856f91c27acb489c227963526ec52db90c7363ed
iliyankg <iliyankgeorgiev@gmail.com>
2015-11-25 10:13:47 +0000

    Bug fix

--- Jscript/NetworkController.js
+++ Jscript/NetworkController.js
@@ -2,7 +2,7 @@ var clientID = '247812372074-s6v0cjlf12q04cm7gbajipmfv1ee9iu8';
 var apiKey = 'AIzaSyCCaQb_poHUrGjOjX34w7uZKhpuY_NZOqc';
 var scope = 'https://www.googleapis.com/auth/games'; //Unused
 
-var localPlayer;
+//var localPlayer;
 
 handleAuthResult = function(auth)
 {
