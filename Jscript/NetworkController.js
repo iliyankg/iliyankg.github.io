ca1a45b256755b3d28ddb0d87db852ab943d32eb
iliyankg <iliyankgeorgiev@gmail.com>
2015-11-27 22:13:08 +0000

    test

--- Jscript/NetworkController.js
+++ Jscript/NetworkController.js
@@ -64,7 +64,7 @@ createJoinGame = function()
         {
             "kind" : "games#turnBasedMatchCreateRequest",
             /*"variant" : 0,*/
-            "invitedPlayerIds": ["me"],
+            /*"invitedPlayerIds": [],*/
             "autoMatchingCriteria" :
             {
                 "kind" : "games#turnBasedAutoMatchingCriteria",
