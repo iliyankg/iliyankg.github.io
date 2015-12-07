c2c7b3ce1b1a1b9c63dedfc78a49c2d647945c1e
Gustavo Sanches <sanchesg@EC214-ECWS-23.coventry.ac.uk>
2015-12-07 13:30:31 +0000

    fix

--- Jscript/View.js
+++ Jscript/View.js
@@ -18,7 +18,7 @@ canvas.height = gameHeight;
 */
 function View()
 {
-    this._rectsMenu = [{x: canvas.style.width/2 - 200, y: canvas.style.height/2 + 100, w: 150, h: 50, t: "play"},//Play game
+    this._rectsMenu = [{x: currentWidth/2 - 200, y: currentHeight/2 + 100, w: 150, h: 50, t: "play"},//Play game
                    {x: canvas.style.width/2 + 50, y: canvas.style.height/2 + 100, w: 150, h: 50, t: "google"}];//Google play
     this._rectsMinionsLocal = [{x: canvas.width/2 - 75, y: canvas.height - 125, w: 50, h: 50, t: "firstLS", m: null},
                               {x: canvas.width/2 - 25, y: canvas.height - 125, w: 50, h: 50, t: "secondLS", m: null},
